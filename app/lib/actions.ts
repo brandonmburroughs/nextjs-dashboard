7a082b85d7ee1f89d8c1d15323f3c5adfbb489d6
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2025-01-24 19:13:01 -0800

    Fixing lint errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -57,6 +57,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     // If a database error occurs, return a more specific error.
     return {
@@ -95,6 +96,7 @@ export async function updateInvoice(
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
     `;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
